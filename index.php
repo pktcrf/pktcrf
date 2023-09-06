a80ae47ccfe661beb98547c7e5f2966d48dd8f32
pktcrf <144210775+pktcrf@users.noreply.github.com>
2023-09-06 15:03:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,110 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CCTV Request Form</title>
+  <!-- Add Bootstrap CSS link -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!-- Add custom styles -->
+  <style>
+    /* Your existing styles here */
+
+    /* Add some spacing to the status message */
+    .status {
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container mt-5">
+    <h2>CCTV Request Form</h2>
+    <!-- Display status message -->
+    <?php if (isset($status)) { ?>
+      <div class="status <?php echo $status['class']; ?>"><?php echo $status['message']; ?></div>
+    <?php } ?>
+
+    <form action="" method="post">
+      <div class="form-group">
+        <label for="department">Department:</label>
+        <input type="text" class="form-control" id="department" name="department" required>
+      </div>
+      <div class="form-group">
+        <label for="requester">Requester:</label>
+        <input type="text" class="form-control" id="requester" name="requester" required>
+      </div>
+      <div class="form-group">
+        <label for="serialNo">Serial No:</label>
+        <input type="text" class="form-control" id="serialNo" name="serialNo" required>
+      </div>
+      <div class="form-group">
+        <label for="contactNo">Contact No:</label>
+        <input type="tel" class="form-control" id="contactNo" name="contactNo" required>
+      </div>
+      <div class="form-group">
+        <label for="incidentDate">Incident Date:</label>
+        <input type="datetime-local" class="form-control" id="incidentDate" name="incidentDate" required>
+      </div>
+      <div class="form-group">
+        <label for="area">Area:</label>
+        <input type="text" class="form-control" id="area" name="area" required>
+      </div>
+      <div class="form-group">
+        <label for="witness">Witness (if any):</label>
+        <input type="text" class="form-control" id="witness" name="witness">
+      </div>
+      <div class="form-group">
+        <label for="date">Date:</label>
+        <input type="date" class="form-control" id="date" name="date" required>
+      </div>
+      <div class="form-group">
+        <label for="estimatedStartTime">Estimated Start Time:</label>
+        <input type="time" class="form-control" id="estimatedStartTime" name="estimatedStartTime" required>
+      </div>
+      <div class="form-group">
+        <label for="estimatedEndTime">Estimated End Time:</label>
+        <input type="time" class="form-control" id="estimatedEndTime" name="estimatedEndTime" required>
+      </div>
+      <div class="form-group">
+        <label for="reason">Reason/Complaint:</label>
+        <textarea class="form-control" id="reason" name="reason" rows="4" required></textarea>
+      </div>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#confirmationModal">Submit</button>
+    </form>
+  </div>
+
+  <!-- Submission Confirmation Modal -->
+  <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="confirmationModalLabel">Confirm Submission</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Are you sure you want to submit this request?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+          <button type="button" class="btn btn-primary" onclick="submitForm()">Confirm</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Add Bootstrap JS and any other scripts if needed -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <script>
+    function submitForm() {
+      // Submit the form programmatically when the confirmation button is clicked
+      document.querySelector('form').submit();
+    }
+  </script>
+</body>
+</html>
